1465767b89e98153d2a16c73fb9233db0aeb581f
John Oenga Ongwae <johnongwae4@gmail.com>
2024-04-17 20:05:56 +0300

    List all implementation

--- src/main/java/io/github/oengajohn/sakilareactive/repository/FilmRepository.java
+++ src/main/java/io/github/oengajohn/sakilareactive/repository/FilmRepository.java
@@ -1,11 +1,11 @@
 package io.github.oengajohn.sakilareactive.repository;
 
-import org.springframework.data.repository.reactive.ReactiveSortingRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import io.github.oengajohn.sakilareactive.entity.Film;
 import reactor.core.publisher.Flux;
 
-public interface FilmRepository extends ReactiveSortingRepository<Film, Integer> {
+public interface FilmRepository extends ReactiveCrudRepository<Film, Integer> {
 
     Flux<Film> findAllBy();
 
